25730109435a0bfd309d9acc2bf199b366ee30b6
DFBlok <s219392862@mandela.ac.za>
2025-07-01 20:30:49 +0200

    added prop for Experience section

---
+++ app/components/ExperineceCard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const ExperineceCard = ({
+  image,
+  title,
+  company,
+  location,
+  period,
+  type,
+  description,
+}: any) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-6 flex flex-col md:flex-row gap-6 items-start mb-6">
+      <div className="flex-shrink-0 w-24 h-24">
+        <img
+          src={image}
+          alt={`${company} logo`}
+          className="w-full h-full object-contain"
+        />
+      </div>
+
+      <div className="flex-1">
+        <h6 className="text-xl font-semibold mb-1">{title}</h6>
+        <p className="text-sm text-gray-500 mb-2">
+          {company} | {location} | {period}
+        </p>
+        <span className="inline-block bg-green-100 text-green-800 text-xs px-3 py-1 rounded-full mb-3">
+          {type}
+        </span>
+        <p className="text-gray-700 leading-relaxed">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ExperineceCard;
--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -1,37 +1,31 @@
-import React from 'react'
+import React from "react";
+import ExperineceCard from "./ExperineceCard";
 
-const education = () => {
+const experienceData = [
+  {
+    image: "/a1.png",
+    title: "Front-End Developer",
+    company: "WeLab",
+    location: "Remote",
+    period: "Jan 2019 - Present",
+    type: "Full Time",
+    description:
+      "Worked on developing responsive UI components and integrating REST APIs. Contributed to performance optimization and cross-browser compatibility fixes.",
+  },
+];
+
+const Experience = () => {
   return (
-<div className='mb-8 '>
-          <h1 className="relative text-4xl font-bold mt-8 mb-8 uppercase inline-block after:block after:content-[''] after:w-16 after:h-1 after:bg-green-500 after:mx-auto after:mt-2">{'>'}Experience</h1>
-          
-          <div className='resume-box'>
-            <div className='resume-row'>
-              <div className='row'>
-                <div>
-                  <div className='rb-left'>
-                    <img src="/a1.png" alt="" />
-                  </div>
-                  <div className='col-sm-9 col-m-xl-10'>
+    <div>
+      <h1 className="relative text-4xl font-bold mt-8 mb-8 uppercase inline-block after:block after:content-[''] after:w-16 after:h-1 after:bg-green-500 after:mx-auto after:mt-2">
+        {">"}Experience
+      </h1>
 
-                  </div>
-                  <div className='rb-right'>
-                    <h6>Front-End Developer</h6>
-                    <label>WeLab | Remote | Jan 2019 - Present </label>
-                    <div className='rb-time'>
-                      Full Time
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
-                  </div>
-                </div>
-                
+      {experienceData.map((exp, index) => (
+        <ExperineceCard key={index} {...exp} />
+      ))}
+    </div>
+  );
+};
 
-              </div>
-            </div>
-          </div>
-
-        </div>
-  )
-}
-
-export default education
+export default Experience;
